53c7bab4573256ea80eb8181f9f7019f9001a443
allan borba <allan.borba@prevision.com.br>
2023-02-09 15:32:00 -0300

    products factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :product do
     description { "MyString" }
     unit_price { 1.5 }
+    width { 3.0 }
+    height { 2.0 }
+    depth { 1.0 }
+    weight { 1.0 }
   end
 end
