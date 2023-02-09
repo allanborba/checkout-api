d3a79af08e0743fb320a1e8a5d6214cfbc3f2a10
allan borba <allan.borba@prevision.com.br>
2023-02-09 14:52:20 -0300

    product and spec add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,24 @@ class Product < ApplicationRecord
   before_save :validate_arguments
   before_update :validate_arguments
 
+  private
+
   def validate_arguments
+    validate_type
+
+    raise ArgumentError, "Unit price must be greater then zero" unless unit_price > 0.0
+    raise ArgumentError, "Width must be greater then zero" unless width > 0.0
+    raise ArgumentError, "Height must be greater then zero" unless height > 0.0
+    raise ArgumentError, "Depth must be greater then zero" unless depth > 0.0
+    raise ArgumentError, "Weight must be greater then zero" unless weight > 0.0
+  end
+
+  def validate_type
     raise ArgumentError, "Description must be a String" unless description.is_a? String
     raise ArgumentError, "Unit price must be a Float" unless unit_price.is_a? Float
-    raise ArgumentError, "Unit price must be greater then zero" unless unit_price > 0.0
+    raise ArgumentError, "Width must be a Float" unless width.is_a? Float
+    raise ArgumentError, "Height must be a Float" unless height.is_a? Float
+    raise ArgumentError, "Depth must be a Float" unless depth.is_a? Float
+    raise ArgumentError, "Weight must be a Float" unless weight.is_a? Float
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,9 +2,22 @@ require "rails_helper"
 
 RSpec.describe Product do
   let(:product) { described_class.new(params).save }
-  let(:params) { { description: description, unit_price: unit_price } }
+  let(:params) do
+    {
+      description: description,
+      unit_price: unit_price,
+      width: width,
+      height: height,
+      depth: depth,
+      weight: weight
+    }
+  end
   let(:description) { "Item description" }
   let(:unit_price) { 5.0 }
+  let(:width) { 1 }
+  let(:height) { 1.0 }
+  let(:depth) { 1.0 }
+  let(:weight) { 1.0 }
 
   describe "#validate" do
     context "when description is invalid" do
@@ -19,10 +32,52 @@ RSpec.describe Product do
       it { expect { product }.to raise_error(ArgumentError, "Unit price must be a Float") }
     end
 
+    context "when width is invalid" do
+      let(:width) { nil }
+
+      it { expect { product }.to raise_error(ArgumentError, "Width must be a Float") }
+    end
+    context "when height is invalid" do
+      let(:height) { nil }
+
+      it { expect { product }.to raise_error(ArgumentError, "Height must be a Float") }
+    end
+    context "when depth, is invalid" do
+      let(:depth) { nil }
+
+      it { expect { product }.to raise_error(ArgumentError, "Depth must be a Float") }
+    end
+    context "when weight, is invalid" do
+      let(:weight) { nil }
+
+      it { expect { product }.to raise_error(ArgumentError, "Weight must be a Float") }
+    end
+
     context "when unit price is not a valid number" do
       let(:unit_price) { -1.0 }
 
       it { expect { product }.to raise_error(ArgumentError, "Unit price must be greater then zero") }
+    end
+
+    context "when width is not a valid number" do
+      let(:width) { -1.0 }
+
+      it { expect { product }.to raise_error(ArgumentError, "Width must be greater then zero") }
+    end
+    context "when height is not a valid number" do
+      let(:height) { -1.0 }
+
+      it { expect { product }.to raise_error(ArgumentError, "Height must be greater then zero") }
+    end
+    context "when depth is not a valid number" do
+      let(:depth) { -1.0 }
+
+      it { expect { product }.to raise_error(ArgumentError, "Depth must be greater then zero") }
+    end
+    context "when weight is not a valid number" do
+      let(:weight) { -1.0 }
+
+      it { expect { product }.to raise_error(ArgumentError, "Weight must be greater then zero") }
     end
 
     context "when has valid params" do
